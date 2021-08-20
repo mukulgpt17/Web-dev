f9b992c1092cee8cc3d99a6ab70b92a6230dd5d8
Mukul_Gupta <mukulgpt17@gmail.com>
2021-08-20 10:35:15 +0530

    adding changes to simon game js

---
+++ Simon Game Challenge Starting Files/games.js
@@ -0,0 +1,51 @@
+alert("hello");
+
+button_col=["red","blue","green","yellow"];
+
+function nextSeq()
+{
+
+    return Math.floor(Math.random() * 4);
+}
+
+$("#green").click(
+        
+    function ()
+    {
+        var gs=new Audio("sounds/blue.mp3");
+    }
+)
+
+$("#red").click(
+        
+    function ()
+    {
+        var rs=new Audio("sounds/blue.mp3");
+    }
+ )
+
+$("#blue").click(
+        
+    function ()
+    {
+        var bs=new Audio("sounds/blue.mp3");
+    }
+ )
+
+        
+$("#yellow").click(
+
+    function ()
+    {
+        var ys=new Audio("sounds/blue.mp3");
+    }
+)
+            
+
+    
+
+
+
+
+
+
